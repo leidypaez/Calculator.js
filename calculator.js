087a0ba1ef93d4d15e22329c037fb683b53460f5
leidypaez <leidy16paez@gmail.com>
2021-10-05 19:53:28 -0500

    se suben modificaciones de eslint

--- calculator.js
+++ calculator.js
@@ -1,38 +1,37 @@
-let numberScreen= "0";
-let screenWithNumber= "si"; //Si o No
-let userPoint= "no"; //Si o No
-let numberWaiting= 0;
-let operationPending= "";
-let solution= "";
+let numberScreen = "0";
+let screenWithNumber = "si"; //Si o No
+let userPoint = "no"; //Si o No
+let numberWaiting = 0;
+let operationPending = "";
+let solution = "";
 
-const enterNumber = (x) => {
-  if(x!=".") {
-		if(numberScreen=="0" || screenWithNumber=="si") {
-			document.Calculadora.txtboxnros.value=x;
-			numberScreen=x;
-		} else if(x!=".") {
-			document.Calculadora.txtboxnros.value+=x;
-			numberScreen+=x;
+const enterNumber = (numbers) => {
+  if(numbers != ".") {
+		if(numberScreen == "0" || screenWithNumber == "si") {
+			document.Calculadora.valueNumber.value = numbers;
+			numberScreen = numbers;
+		} else if(numbers != ".") {
+			document.Calculadora.valueNumber.value += numbers;
+			numberScreen += numbers;
 		}
-  } if(x=="." && userPoint=="no" && numberScreen=="0") {
-			document.Calculadora.txtboxnros.value="0.";
-			numberScreen=x;
+  } if(numbers =="." && userPoint=="no" && numberScreen == "0") {
+			document.Calculadora.valueNumber.value = "0.";
+			numberScreen = numbers;
 			userPoint="si";
-		} else if(x=="." && userPoint=="no") {
-			document.Calculadora.txtboxnros.value +=x;
-			numberScreen +=x;
-			userPoint="si";
-		} else if(x =="." && userPoint =="si") {
+		} else if(numbers == "." && userPoint == "no") {
+			document.Calculadora.valueNumber.value += numbers;
+			numberScreen += numbers;
+			userPoint = "si";
+		} else if(numbers == "." && userPoint == "si") {
   }
 	screenWithNumber = "no";
 }
 
-const enterOperation = (y) => {
-
+const enterOperation = (addValue) => {
   if( operationPending == "") {
-		numberWaiting =document.Calculadora.txtboxnros.value;
-		document.Calculadora.txtboxnros.value +=y;
-		operationPending = y;
+		numberWaiting = document.Calculadora.valueNumber.value;
+		document.Calculadora.valueNumber.value += addValue;
+		operationPending = addValue;
 		screenWithNumber = "no";
 		numberScreen = "";
 		userPoint = "no";
@@ -40,34 +39,22 @@ const enterOperation = (y) => {
 }
 
 const result = () => {
-
   if(operationPending != "") {
-		solution=numberWaiting+operationPending+numberScreen;
-		document.Calculadora.txtboxnros.value=eval(solution);
-		numberScreen=eval(solution);
-		screenWithNumber="si";
-		operationPending = "";
-		userPoint = "no";
-  }
-}
-
-const raiz = () => {
-
-  if(operationPending == "") {
-		document.Calculadora.txtboxnros.value=Math.sqrt(numberScreen);
-		screenWithNumber = "no";
+		solution = numberWaiting+operationPending + numberScreen;
+		document.Calculadora.valueNumber.value=eval(solution);
+		numberScreen = eval(solution);
+		screenWithNumber ="si";
 		operationPending = "";
 		userPoint = "no";
   }
 }
 
 const limpiarValue = () => {
-
-	numberScreen= "0";
-	screenWithNumber= "si";
-	userPoint= "no"; 
-	numberWaiting= 0;
-	operationPending= "";
-	solution= "";
-	document.Calculadora.txtboxnros.value= "0";
+	numberScreen = "0";
+	screenWithNumber = "si";
+	userPoint = "no";
+	numberWaiting = 0;
+	operationPending = "";
+	solution = "";
+	document.Calculadora.valueNumber.value = "0";
 }
